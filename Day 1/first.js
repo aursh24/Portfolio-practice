d2d9d9f1ff6c8e1c2f39c0e31140e53b4af99603
aursh <aursh.headstrong24@gmail.com>
2024-10-19 20:09:27 +0530

    code_dump

---
+++ Day 1/first.js
@@ -0,0 +1,16 @@
+// console.log("Hello1");
+// setTimeout(function(){
+//     console.log("Hello3");
+// }, 2000);
+// console.log("Hello2");
+
+// setInterval(function(){
+//     console.log("Hello 4");
+// },2000);
+// console.log("Hello 5");
+
+console.log("Hello1");
+setTimeout(function(){
+    console.log("Hello2");
+},0);
+console.log("Hello3");
